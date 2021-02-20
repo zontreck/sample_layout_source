47cdd9e7f0d92933e58aaf3a2961805e62c385d0
zontreck <tarapiccari@gmail.com>
2021-02-20 05:43:53 +0000

    Deploying to gh-pages from @ zontreck/sample_layout_source@8f1d57daff616d473496fdd3531ac5f294f2d94c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d16ea6bab1aebc3beb62582e7d7f7169.js"
+  "/precache-manifest.075280a0f2a3040e574383b1c6963bce.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
