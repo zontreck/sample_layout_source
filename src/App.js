76e948666c8e543b62d85e5bc048decfb64bc273
zontreck <tarapiccari@gmail.com>
2021-03-12 21:34:30 -0700

    import usestate in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import AppWelcome from "./AppWelcome";
 import NotFound from "./NotFound";
